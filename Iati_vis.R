56be84200e445a466ee5644d28a6f9dd9ae0a246
emcbride09 <elliot.a.mcbride@gmail.com>
2019-03-15 11:13:16 +0000

    first plots
    first iterations of plots

--- Iati_vis.R
+++ Iati_vis.R
@@ -30,22 +30,25 @@ options(scipen = 10)
 
 plot1 <- ggplot(n, aes(x = orgttype, xend = orgttype)) +
   labs(title = 'Donor Funds Earmarking 2015-2018', 
-       subtitle = 'Breakdown of Earmarking Type From Initial Funds Release', 
+       subtitle = 'Breakdown of Earmarking Type From Donor Funds by Year', 
        x = 'Organisation Type',
        y = '') 
 
 #budget bars with low alpha
 
-plot2 <- plot1 + geom_segment(aes(y = annualtotal, yend=0, color= "Total budget"), alpha = 0.2, size = 30) +
+plot2 <- plot1 + geom_segment(aes(y = annualtotal, yend=0, color= "Total budget"), alpha = 0.2, 
+                              size = 15) +
   scale_color_manual(values = '#CEE0DC')
 
 #earmarking bars
 
-gm <- plot2 + geom_col(aes(y = emamount, fill = earmarkingtype), size = 5) +
-  scale_fill_manual(values = c('#DB2B39', '#99D5C9', '#3AAFB9', '#074F57', '#CEE0DC'))
+gm <- plot2 + geom_col(aes(y = emamount, fill = earmarkingtype), width = 0.2) +
+  scale_fill_manual(values = c('#cae7b9', '#f3de8a', '#eb9486', '#7e7f9a', '#97a7b3'), 
+                    name = 'Earmarking Level')
 
 iati_plot_base <- gm + facet_grid(cols = vars(year)) +
-  scale_y_continuous(breaks = c(10000000, 20000000, 30000000), labels = c("$10 million", "$20 million", "$30 million")) 
+  scale_y_continuous(breaks = c(10000000, 20000000, 30000000), 
+                     labels = c("$10 million", "$20 million", "$30 million")) 
 
 
 #theme for bar plot
@@ -60,10 +63,76 @@ iati_plot_base + theme(
   panel.grid.major.x = element_blank(),
   panel.grid.minor.x = element_blank(),
   strip.background = element_rect(fill = '#8AA1B1'),
-  strip.text = element_text(size = 20, family = 'Arial')
+  strip.text = element_text(size = 20, family = 'Arial'),
+  legend.background = element_blank(),
+  legend.key = element_blank(),
+  plot.title = element_text(size = 30),
+  plot.subtitle = element_text(size = 16, face = 'italic'),
+  axis.text = element_text(size = 10),
+  axis.title = element_text(size = 20),
+  panel.border = element_rect(colour="black",size=1, fill = NA)
   
-) + guides(colour = guide_legend(overide.aes = list(size = 1)))
+) + guides(colour = guide_legend(title = NULL,
+                                 order = 0,
+                                 override.aes = list(size = 7)
+                                 ),
+           fill = guide_legend(order = 1)
+           )
+
+ggsave("bar_by_year.png", plot = last_plot, width = 1300, height = 681, units = 'mm') 
+
+#------------------------------------------
+
+
+
+unem <- n %>% filter(earmarkingtype == 'Unearmarked')
+tightly <- n %>% filter(earmarkingtype == 'Tightly earmarked')
+
+line_iati <- unem %>% ggplot(aes(x = year, y = emamount, fill = orgttype)) + geom_col(position = 'dodge') +
+  labs(title = 'Unearmarked funds', subtitle = 'Funds by organisation that are delivered without any prescriptive earmarking')
+ 
+
+line_iati
+
+emtype <- unique(n$earmarkingtype)
+
+for (i in emtype){
   
+  options(scipen = 10)
   
+  df <- n %>% filter(earmarkingtype == i)
   
+  print(ggplot(df, aes(x = year, y = emamount, fill = orgttype)) + geom_col(position = 'dodge') +
+          scale_fill_manual(values = c('#cae7b9', '#f3de8a', '#eb9486', '#7e7f9a', '#97a7b3')) +
+    labs(title = i, subtitle = paste('Funds by organisation that are issued with an earmarking level of:', i),
+         x = 'Year', y = 'Earmarked funds') +
+      theme(axis.text.x = element_text(vjust = 1, hjust = 1, family = 'Arial', size = 11, color = 'Black'),
+        axis.text.y = element_text(family = 'Arial', size = 11, color = 'Black'),
+        plot.background = element_rect(fill = 'grey85'),
+        panel.background = element_rect(fill = 'grey85'),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        strip.background = element_rect(fill = '#8AA1B1'),
+        strip.text = element_text(size = 20, family = 'Arial'),
+        legend.background = element_blank(),
+        legend.key = element_blank(),
+        plot.title = element_text(size = 30),
+        plot.subtitle = element_text(size = 16, face = 'italic'),
+        axis.text = element_text(size = 10),
+        axis.title = element_text(size = 20),
+        panel.border = element_rect(colour="black",size=1, fill = NA)
+        
+      ) + guides(fill = guide_legend(title = 'Org Type',
+                                       order = 0,
+                                       override.aes = list(size = 7)
+      )
+    )
+  )
+  ggsave(filename = paste(i,".png"), plot = last_plot())
+}
+
+
+
 
